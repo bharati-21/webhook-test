da7d0bab0a416299dc56a85d628364e17b979ae2
Bharati Subramanian <bharatisharada@gmail.com>
2023-05-15 09:37:20 +0530

    feat: webhook test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const fetchIssues = async () => {
+		try {
+			const res = await axios.get(
+				"https://webhook-test.bhaaratii.repl.co/issues"
+			);
+			console.log(res.data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+    // const TOKEN = "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";
+
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<button onClick={fetchIssues}>Fetch Issues</button>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
+};
 
 export default App;
